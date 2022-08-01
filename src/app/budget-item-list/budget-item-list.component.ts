532fc09a7b8e2ed6b7c95b01b1ea6d0242348cd2
santiago julian monsalve <s.julian.monsalve@accenture.com>
2022-08-01 13:47:48 +0200

    properly updating main amount after editing from module

--- src/app/budget-item-list/budget-item-list.component.ts
+++ src/app/budget-item-list/budget-item-list.component.ts
@@ -8,9 +8,12 @@ import { EditItemModalComponent } from '../edit-item-modal/edit-item-modal.compo
   templateUrl: './budget-item-list.component.html',
   styleUrls: ['./budget-item-list.component.scss']
 })
+
 export class BudgetItemListComponent implements OnInit {
+
   @Input() budgetItems!: BudgetItem[];
   @Output() delete: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
+  @Output() update: EventEmitter<UpdateEvent> = new EventEmitter<UpdateEvent>();
 
   constructor(public dialog: MatDialog) { }
 
@@ -29,9 +32,18 @@ export class BudgetItemListComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(result => {
       if(result) {
-        this.budgetItems[this.budgetItems.indexOf(item)] = result
+        //this.budgetItems[this.budgetItems.indexOf(item)] = result
+        this.update.emit({
+          old: item,
+          new: result
+        })
       }
     })
   }
 
+}
+
+export interface UpdateEvent {
+  old: BudgetItem;
+  new: BudgetItem;
 }
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BudgetItem } from 'src/shared/models/budget-item.model';
+import { UpdateEvent } from '../budget-item-list/budget-item-list.component';
 
 @Component({
   selector: 'app-main-page',
@@ -25,6 +26,13 @@ export class MainPageComponent implements OnInit {
     let index = this.budgetItems.indexOf(item);
     this.budgetItems.splice(index, 1);
     this.totalBudget -= item.amount;
+  }
+
+  updateItem(updateEvent: UpdateEvent) {
+    this.budgetItems[this.budgetItems.indexOf(updateEvent.old)] = updateEvent.new;
+
+    this.totalBudget -= updateEvent.old.amount;
+    this.totalBudget += updateEvent.new.amount;
   }
 
 }
